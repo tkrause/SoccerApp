57c2c78b623eada7c42fe9bf8d7b09143bc2f9f0
barajas90 <37250636+barajas90@users.noreply.github.com>
2019-09-07 14:58:15 -0700

    Added more to signup

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,22 +1,35 @@
 <template>
 
     <Page class="page" >
-        <ActionBar title="Login" class="action-bar" />
+        <ActionBar title="Sign Up" class="action-bar" />
         <ScrollView>
 
-            <StackLayout class="home-panel">
-                <!--Add your page content here-->
-                <Label text="WE did it!"></Label>
+                            <StackLayout class="home-panel">
+                                <!--Add your page content here-->
+                                <TextField v-model="Name" id="Name"
+                                           hint="Name" autocorrect="false" autocapitalizationType="none" />
+                                <TextField v-model="email" id="email"
+                                           hint="email" autocorrect="false" autocapitalizationType="none" />
+                                <TextField v-model="password" secure="true"
+                                           hint="Password" />
+                                <TextField v-model="password2" secure="true"
+                                                                   hint="Re-enter Password" />
+                                <Button class="Create" text="Create Account" @tap="onSignIn" />
 
-            </StackLayout>
-        </ScrollView>
+
+                            </StackLayout>
+                </ScrollView>
     </Page>
 </template>
 
 
 <script>
     export default {
-        name: "SignUp"
+        Name:null,
+        email:null,
+        password:null,
+        password2:null,
+
     }
 </script>
 
