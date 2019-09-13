9863c64a56996a48f10ca15854a78d290acc1571
Thomas Krause <trueswordmaster@gmail.com>
2019-09-13 14:01:09 -0700

    TabView for William

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,36 @@
 <template>
     <Page class="page">
         <ActionBar class="action-bar" title="Soccer League"></ActionBar>
-        <Label class="font-weight-bold">Welcome {{ $api.current_user.name }}</Label>
+
+        <TabView
+            class="tab-view"
+            androidTabsPosition="bottom">
+
+            <TabViewItem class="tab-view-item" title="Tab 1">
+                <Teams></Teams>
+            </TabViewItem>
+
+            <TabViewItem class="tab-view-item" title="Tab 2">
+                <Label text="Content for Tab 2" />
+            </TabViewItem>
+        </TabView>
+<!--        <Label class="font-weight-bold">Welcome {{ $api.current_user.name }}</Label>-->
     </Page>
 </template>
 
 <script>
+    import Teams from "../tabs/Teams";
+
     export default {
         name: "Home",
+        components: {
+            Teams
+        }
     }
 </script>
+
+<style>
+    .action-bar {
+        color: #ff9900;
+    }
+</style>
---
+++ src/tabs/Teams.vue
@@ -0,0 +1,13 @@
+<template>
+    <Label text="Welcome to my teams" />
+</template>
+
+<script>
+    export default {
+        name: "Teams"
+    }
+</script>
+
+<style scoped>
+
+</style>
